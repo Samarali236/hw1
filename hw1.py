88df603ea5f9cfe584a842dc4b06c29492665751
Samar Ali <samarali27698@gmail.com>
2025-06-16 14:34:38 +0300

    Initializing the ATM menue of choices with a Loop

--- hw1.py
+++ hw1.py
@@ -1,1 +1,12 @@
+balance =[1000] 
+choice=0
 
+
+while choice !=4 :
+    print("""
+    Welcome to the ATM
+    1. Check Balance
+    2. Deposit Money
+    3. Withdraw Money
+    4. Exit
+          """)
