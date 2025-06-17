d1cd2218641d6b2d499aef0720d65cec39e84d26
Samar Ali <samarali27698@gmail.com>
2025-06-17 16:31:33 +0300

    Fixing the code in order to add a logout option

--- hw1.py
+++ hw1.py
@@ -6,54 +6,58 @@ faildwithcount=0
 password= "qwerty"
 userpass=""
 
-while True:
-    userpass=input("Please enter your password: ")
+while choice !=6:
+    userpass = input("Please enter your password: ")
     if userpass == password :
+     choice = 0
      print("login successfully! ")
-     break
-    else:
-     print("Incorrect password, try another password")
-
-while choice !=5 :
-    print("""
-    Welcome to the ATM
-    1. Check Balance
-    2. Deposit Money
-    3. Withdraw Money
-    4. Transaction count
-    5. Exit
+    while choice !=5 :
+        print("""
+        Welcome to the ATM
+        1. Check Balance
+        2. Deposit Money
+        3. Withdraw Money
+        4. Transaction count
+        5. Logout
+        6. Exit system
           """)
-    choice=int(input("please enter your choice: "))
-    if choice ==1:
-        print("Your balance is: "+str(balance)+"$")
-    elif choice==2:
-        deposite= float(input("Please enter the amount you would like to deposit: "))
-        if deposite > 0:
-            balance += deposite
-            transcount+=1
-            print("Your current balance is: "+str(balance)+"$")
-        else:
-            print("Please enter a valid deposite amount!")
-    elif choice==3:
-        withdraw = float(input("Please enter the amount you would like to withdraw: "))
-        if withdraw > 0 and withdraw <= balance:
-            balance -= withdraw
-            transcount += 1
-            print("Your current balance is: "+str(balance)+"$")
-        else:
-            faildwithcount+=1
-            if withdraw > balance:
-                 print("Dear user,you do not have enough funds for this withdrawal, please check your balance or try a smaller amount")
-            if withdraw < 0:
-                 print("Dear User, Please enter a valid amount for withdrawwal process")
-            if faildwithcount>3:
+        choice=int(input("please enter your choice: "))
+        if choice ==1:
+            print("Your balance is: "+str(balance)+"$")
+        elif choice==2:
+            deposite= float(input("Please enter the amount you would like to deposit: "))
+            if deposite > 0:
+                balance += deposite
+                transcount+=1
+                print("Your current balance is: "+str(balance)+"$")
+            else:
+                print("Please enter a valid deposite amount!")
+        elif choice==3:
+            withdraw = float(input("Please enter the amount you would like to withdraw: "))
+            if withdraw > 0 and withdraw <= balance:
+                balance -= withdraw
+                transcount += 1
+                print("Your current balance is: "+str(balance)+"$")
+            else:
+                faildwithcount+=1
+                if withdraw > balance:
+                    print("Dear user,you do not have enough funds for this withdrawal, please check your balance or try a smaller amount")
+                if withdraw < 0:
+                    print("Dear User, Please enter a valid amount for withdrawwal process")
+                if faildwithcount>3:
                     print("WARNING! more than 3 failed withdrawals were attempted")
-    elif choice==4:
-        print(" Dear user, your total number of successfull transactions is : "+ str(transcount))
-    elif choice==5:
-        print("program ended, Thank you for using our ATM")
+        elif choice==4:
+            print(" Dear user, your total number of successfull transactions is : "+ str(transcount))
+        elif choice==5:
+            print("Logging out")
+            break
+        elif choice == 6:
+            print("program ended, Thank you for using our ATM")
+            break
+        else:
+            print("Invalid number, Please choose between 1 and 5")
     else:
-        print("Invalid number, Please choose between 1 and 5")
+        print("Incorrect password, try another password")
 
 
     
