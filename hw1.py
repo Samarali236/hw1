17b72dd692083392e0afc7446a700ddff7e56bb2
Samar Ali <samarali27698@gmail.com>
2025-06-17 14:58:09 +0300

    Implementing the Transaction count option

--- hw1.py
+++ hw1.py
@@ -1,13 +1,15 @@
 balance =[1000] 
 choice=0
 deposite=0
-while choice !=4 :
+transcount=0
+while choice !=5 :
     print("""
     Welcome to the ATM
     1. Check Balance
     2. Deposit Money
     3. Withdraw Money
-    4. Exit
+    4. Transaction count
+    5. Exit
           """)
     choice=int(input("please enter your choice: "))
     if choice ==1:
@@ -15,15 +17,24 @@ while choice !=4 :
     elif choice==2:
         deposite=float(input("Please enter the amount you would like to deposit: "))
         balance[0] += deposite
+        transcount+=1
         print("Your current balance is: "+str(balance[0])+"$")
     elif choice==3:
         withdraw=float(input("Please enter the amount you would like to withdraw: "))
-        if  withdraw <= balance[0]  :
+        if  withdraw<= balance[0]:
             balance[0]-=withdraw
+            transcount+=1
             print("Your current balance is: "+str(balance[0])+"$")
         else:
             print("Dear user,you do not have enough funds for this withdrawal, please check your balance or try a smaller amount")
-
+            
     elif choice==4:
-        print("program ended")
-        
+        print(" Dear user, your total number of successfull transactions is : "+ str(transcount))
+    elif choice==5:
+        print("program ended, Thank you for using our ATM")
+    else:
+        print("Invalid number, Please choose between 1 and 5")
+
+
+    
+    
